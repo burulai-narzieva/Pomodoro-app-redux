5ecac1d4efd4b608a1b4e4d3caf3d844cecb6d50
Burulai <burulainarzieva2@gmail.com>
2021-06-30 23:41:30 +0600

    Updates

---
+++ precache-manifest.b72cf98fc9d8a67508495b8c1e8e002c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a52861dfd95285ecabfa954c12c041e",
+    "url": "/Pomodoro-app-redux/index.html"
+  },
+  {
+    "revision": "fd58c3d0880fd568c781",
+    "url": "/Pomodoro-app-redux/static/css/main.f9f168f2.chunk.css"
+  },
+  {
+    "revision": "ccd29bca326eea62401c",
+    "url": "/Pomodoro-app-redux/static/js/2.de9c8c41.chunk.js"
+  },
+  {
+    "revision": "a108088b0317a4b3eaee2a12a54f856c",
+    "url": "/Pomodoro-app-redux/static/js/2.de9c8c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd58c3d0880fd568c781",
+    "url": "/Pomodoro-app-redux/static/js/main.5b63b480.chunk.js"
+  },
+  {
+    "revision": "718f98b4e98b472cdc3d",
+    "url": "/Pomodoro-app-redux/static/js/runtime-main.c8cb3f12.js"
+  },
+  {
+    "revision": "525320df0b1f39bb54c3662d70cde5c6",
+    "url": "/Pomodoro-app-redux/static/media/bell.525320df.flac"
+  }
+]);
